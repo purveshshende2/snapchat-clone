7ea4c8fed98f1b090f5977b1578a7d4cd1612634
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-12-13 16:24:22 +0530

    first commit

---
+++ app/src/main/java/com/example/snapchat/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.snapchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    var emailEditText: EditText? = null
+    var passwordEditText: EditText? = null
+    val auth = Firebase.auth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE) //will hide the title
+        getSupportActionBar()?.hide() // hide the title bar
+        this.getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN) //enable full screen
+        setContentView(R.layout.activity_main)
+
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+
+        if(auth.currentUser != null) {
+            logIn()
+        }
+    }
+
+    fun goClicked(view: View){
+        //check if we can log in the user
+        auth.signInWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                  logIn()
+                } else {
+                    //sign up please!
+                    auth.createUserWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString()).addOnCompleteListener(this) { task ->
+                        if(task.isSuccessful){
+                            // Add to database
+                            logIn()
+                        } else {
+                            Toast.makeText(this,"Login Failed. Try Again",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+
+    }
+
+    fun logIn(){
+       val intent = Intent(this,snapsActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/snapchat/snapsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.snapchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class snapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snaps)
+    }
+}
